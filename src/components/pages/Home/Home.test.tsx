754db52efbaa4c5742b921147eea1f2de2a2d7f7
Inna T <meeoow005@gmail.com>
2023-03-20 21:50:38 +0100

    fix: delete jest

--- src/components/pages/Home/Home.test.tsx
+++ src/components/pages/Home/Home.test.tsx
@@ -1,11 +1,11 @@
-import { Home } from './Home';
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import '@testing-library/jest-dom/extend-expect';
-
-test('renders page title', () => {
-  render(<Home />);
-  const pageTitle = 'Home';
-  const pageTitleElement = screen.getByText(pageTitle);
-  expect(pageTitleElement).toBeInTheDocument();
-});
+// import { Home } from './Home';
+// import { render, screen } from '@testing-library/react';
+// import React from 'react';
+// import '@testing-library/jest-dom/extend-expect';
+//
+// test('renders page title', () => {
+//   render(<Home />);
+//   const pageTitle = 'Home';
+//   const pageTitleElement = screen.getByText(pageTitle);
+//   expect(pageTitleElement).toBeInTheDocument();
+// });
