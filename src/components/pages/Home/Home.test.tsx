98a3b8c9416724770083a50395a2787e9c575e91
Inna T <meeoow005@gmail.com>
2023-03-21 03:20:03 +0100

    feat: add test

--- src/components/pages/Home/Home.test.tsx
+++ src/components/pages/Home/Home.test.tsx
@@ -1,11 +1,15 @@
-// import { Home } from './Home';
-// import { render, screen } from '@testing-library/react';
-// import React from 'react';
-// import '@testing-library/jest-dom/extend-expect';
-//
-// test('renders page title', () => {
-//   render(<Home />);
-//   const pageTitle = 'Home';
-//   const pageTitleElement = screen.getByText(pageTitle);
-//   expect(pageTitleElement).toBeInTheDocument();
-// });
+import { test, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+import App from '../../App';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+test('Should show title', async () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  expect(screen.getByText('Home')).toBeInTheDocument();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
 
 // https://vitejs.dev/config/
+
+const vitestConfig: VitestUserConfigInterface = {
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      include: ['src/**/*.tsx'],
+      exclude: ['src/**/*.spec.js'],
+      provider: 'c8',
+      all: true,
+      skipFull: false,
+      reporter: 'text',
+    },
+  },
+};
 export default defineConfig({
   plugins: [react()],
+  test: vitestConfig.test,
 });
+
