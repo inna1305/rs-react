8e8a51575e210e6c26c3e06859ce8c789f87758f
Inna T <meeoow005@gmail.com>
2023-03-29 09:19:05 +0200

    fix: replace index for cards keys

--- src/components/pages/Home/Cards/Cards.tsx
+++ src/components/pages/Home/Cards/Cards.tsx
@@ -4,9 +4,8 @@ import './cards.css';
 import { Card } from './Card';
 
 const Cards = () => {
-  //TODO заменить ключ
-  const items = persons.map((person) => (
-    <div key={person.name} className="card-container">
+  const items = persons.map((person, index) => (
+    <div key={index} className="card-container">
       <Card {...person} />
     </div>
   ));
