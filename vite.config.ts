2097e20c7f091eb8996d42f4d6f773d3d067b17e
Inna T <meeoow005@gmail.com>
2023-03-31 20:25:35 +0200

    fix: configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import type { UserConfig as VitestUserConfigInterface } from 'vitest/config';
+import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 
@@ -8,13 +8,14 @@ const vitestConfig: VitestUserConfigInterface = {
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './src/helpers/setupTests.ts',
+    css: true,
     coverage: {
-      include: ['src/**/*.tsx'],
-      exclude: ['src/**/*.spec.js'],
+      enabled: true,
       provider: 'c8',
+      reporter: ['text'],
       all: true,
-      skipFull: false,
-      reporter: 'text',
+      exclude: ['*.cjs', 'src/main.tsx', 'src/types.ts', 'src/components/cardsData.ts', '*.d.ts'],
     },
   },
 };
@@ -22,4 +23,3 @@ export default defineConfig({
   plugins: [react()],
   test: vitestConfig.test,
 });
-
