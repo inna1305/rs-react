6478c52b2eb7f5697009621c22b130f23bc48f5f
Inna T <meeoow005@gmail.com>
2023-04-12 00:11:24 +0200

    feat: add modal for cards (not closing)

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -3,11 +3,13 @@ import { IMovie } from '../../types';
 import './cards.css';
 import { ConfigContext } from '../App';
 
+const alternativeImg = '../../../public/alternativePoster.png';
+
 const Card = (props: IMovie) => {
   const { title, id, poster_path, backdrop_path } = props;
   const config = useContext(ConfigContext);
   const [image, setImage] = useState<string | null>(null);
-  const alternativeImg = '../../../public/alternativePoster.png';
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     async function fetchImage() {
@@ -21,8 +23,16 @@ const Card = (props: IMovie) => {
     fetchImage();
   }, [config, backdrop_path, poster_path]);
 
+  const handleOpenCard = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
-    <div className="card-container" id={String(id)}>
+    <div className="card-container" id={String(id)} onClick={handleOpenCard}>
       <div
         className="card-photo"
         style={{
@@ -30,8 +40,39 @@ const Card = (props: IMovie) => {
         }}
       ></div>
       <div className="card-info card-name">{title}</div>
-      <div className="card-info">{}</div>
+      {showModal && (
+        <CardModal
+          image={image}
+          overview={props.overview}
+          onClose={handleCloseModal}
+          title={props.title}
+        />
+      )}
     </div>
   );
 };
 export default Card;
+
+interface IMovieModal {
+  title: string;
+  overview: string;
+  image?: string | null;
+  onClose: () => void;
+}
+
+const CardModal = (props: IMovieModal) => {
+  return (
+    <>
+      <div className="card-container-modal">
+        <div
+          className="card-photo-modal"
+          style={{
+            backgroundImage: `url(${props.image || alternativeImg})`,
+          }}
+        ></div>
+        <div className="card-info card-name">{props.title}</div>
+        <div className="card-info-modal">{props.overview}</div>
+      </div>
+    </>
+  );
+};
